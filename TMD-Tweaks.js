4b5f7ae56b806c429ef9a3c81788054ff93b7113
Sergiu Negara <negara.sergiu@gmail.com>
2012-11-03 02:02:55 +0200

    v. 1.1.0 code rebuilt new feature: forum topic block

--- TMD-Tweaks.js
+++ TMD-Tweaks.js
@@ -1,39 +1,62 @@
 // ==UserScript==
 // @name        TMD Tweaks
 // @description Tweaks for TMD
-// @include	    *torrentsmd.com/*
-// @include	    *torrentsmd.eu/*
+// @include     *torrentsmd.com/*
+// @include     *torrentsmd.eu/*
 // @include     *torrentsmd.me/*
 // @include     *torrentsmoldova.com/*
-// @include	    *torrentsmoldova.org/*
+// @include     *torrentsmoldova.org/*
 // @include     *torrentsmoldova.net/*
-// @version     1.0.3
+// @version     1.1.0
 // ==/UserScript==
 
-jQuery(document).ready(function(){
-	if(jQuery('#user_box').length!=0){
-		var ph      = {};
-		ph.torrents = ['Torente', 'Раздачи'];
-		ph.users    = ['Utilizatori', 'Пользователи'];
-		ph.forum    = ['Forum', 'Форум'];
+$j(document).ready(function(){
+  if($j('#user_box').length!=0){
+    var msg    = {};
+    msg.sf_t   = ['Torente', 'Раздачи'];
+    msg.sf_u   = ['Utilizatori', 'Пользователи'];
+    msg.sf_f   = ['Forum', 'Форум'];
+    msg.topic  = ['Denumirea temei', 'Название темы']
 
-		if(!message.lang) message.lang = 'ro';
-		var l = (message.lang == 'ro') ? 0 : 1;
+    if(!message.lang) message.lang = 'ro';
+    var l      = (message.lang == 'ro') ? 0 : 1;
 
-		var html       = {};
-		html.torrents  = '<form id="search-torrents" action="search.php" method="get"><input class="searchness" type="text" name="search_str" placeholder="' + ph.torrents[l] + '..."></form>';
-		html.users     = '<form id="search-users" action="users.php" method="get"><input class="searchness" type="text" name="search" placeholder="' + ph.users[l] + '..."></form>';
-		html.forum     = '<form id="search-forum" action="forum.php" method="get"><input type="hidden" name="action" value="search"><input class="searchness" type="text" name="keywords" placeholder="' + ph.forum[l] + '..."></form>';
-		html.stylize   = '\
+    var html   = {};
+    html.sf    = {}; // search form
+    html.sf.t  = '<form id="search-torrents" action="search.php" method="get"><input class="searchness" type="text" name="search_str" placeholder="' + msg.sf_t[l] + '..."></form>';
+    html.sf.u  = '<form id="search-users" action="users.php" method="get"><input class="searchness" type="text" name="search" placeholder="' + msg.sf_u[l] + '..."></form>';
+    html.sf.f  = '<form id="search-forum" action="forum.php" method="get"><input type="hidden" name="action" value="search"><input class="searchness" type="text" name="keywords" placeholder="' + msg.sf_f[l] + '..."></form>';
+    html.cnt   = '<div id="search-cntnr">' + html.sf.t + html.sf.u + html.sf.f + '</div>';
+    var style  = '\
 #search-cntnr{ margin: 0 auto; width: 880px; overflow: hidden; padding: 10px 0 20px; }\
 #search-cntnr>*{ float: left; margin-left: 122px; }\
 #search-cntnr>:first-child{ margin-left: 0; }\
 #search-cntnr .searchness{ border: 1px solid #ccc; padding: 2px 5px; width: 200px; }\
 .searchness:focus{ outline: none; }\
 ';
-		html.container = '<div id="search-cntnr">' + html.torrents + html.users + html.forum + '</div>';
-		jQuery('#main_search_text').parent().remove();
-		jQuery('head').append('<style>' + html.stylize + '</style>');
-		jQuery('#no_td_border').after(html.container);
-	}
+    $j('#main_search_text').parent().remove();
+    $j('head').append('<style>' + style + '</style>');
+    $j('#no_td_border').after(html.cnt);
+  }
+
+// topic title block (as is - to be optimized)
+if(location.search.match(/viewtopic/))
+{
+  var topic = $j('title').text().replace(/(^(Vezi tema - )|^(Просмотр темы - )|( ::(.+)$))/g, '');
+  var topichtml = '<div id="topic-title">' + msg.topic[l] + ': ' + topic + '</div>';
+  var topicstyle = '<style>\
+  #topic-title{\
+    background-color: #ECE9D8;\
+    border: 1px solid #A79F72;\
+    padding: 10px;\
+    position: fixed; right: 10px; bottom: 10px;\
+    transition: all .5s ease;\
+  }\
+  #topic-title:hover{\
+    opacity: 0;\
+  }\
+</style>';
+  $j('head').append(topicstyle);
+  $j('body').append(topichtml);
+}
 });
